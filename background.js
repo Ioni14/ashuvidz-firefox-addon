408fedb716734aa55ecba1dcb97f80b3426476c8
vyrtualsynthese <vyrtualsynthese>
2018-04-07 14:27:51 +0200

    cleanup background

--- background.js
+++ background.js
@@ -1,31 +1,21 @@
-
-function twitchCheck(){$.getJSON("https://api.twitch.tv/kraken/streams/Ashuvidz?client_id=yufsibfdh5gwli5nzdj01ceghc1ubv", function(channel) {
+(function () {
+    function twitchCheck(){$.getJSON("https://api.twitch.tv/kraken/streams/Ashuvidz?client_id=yufsibfdh5gwli5nzdj01ceghc1ubv", function(channel) {
   //live is offline Youtube player appened
   if (channel["stream"] == null) {
-    console.log("Stream is offline");
-
-    linkUrl = "http://youtube.com/ashuvidz/";
     browser.browserAction.setIcon({path: "images/offline.png"});
     console.log("set icon offline");
 
     //live is online Twitch player
   } else {
-      linkUrl = "http://twitch.tv/ashuvidz/";
       browser.browserAction.setIcon({path: "images/online.png"});
       console.log("set icon online");
     }
 })};
-var linkUrl = "";
 twitchCheck();
 
 browser.alarms.create('twitchAlarm', {delayInMinutes: 5, periodInMinutes: 5});
 
 browser.alarms.onAlarm.addListener(function( twitchAlarm ) {
   twitchCheck();
-});
-
-browser.browserAction.onClicked.addListener(function(activeTab)
-{
-    var newURL = linkUrl;
-    browser.tabs.create({ url: newURL });
-});
+})}
+)();
