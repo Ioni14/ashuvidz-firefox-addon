116f59731ca254f8ac066f00eed86dca0e6ddca4
vyrtualsynthese <vyrtualsynthese>
2018-04-10 23:28:45 +0200

    fixed for windows version ?

--- background.js
+++ background.js
@@ -4,7 +4,7 @@
         function twitchCheck(){
             $.ajax({
                 url: "https://api.twitch.tv/helix/streams?user_login=ashuvidz",
-                headers: { "Client-ID": "yufsibfdh5gwli5nzdj01ceghc1ubv" },
+                headers: { "Client-ID": "b90nfoacg9807542cq15o2qbv2g05q" },
                 type: "GET",
                 success: function() { console.log('Success!'); }
             }).done(function( channel ) {
@@ -13,6 +13,8 @@
                     if (channel.data.length <= 0) {
                         browser.browserAction.setIcon({path: "images/offline.png"});
                         console.log("set icon offline");
+
+                        linkUrl = "http://youtube.com/ashuvidz/";
 
                         if (isLive === 0) return;
 
@@ -26,6 +28,7 @@
 
                         //live is online Twitch player
                     } else {
+                        linkUrl = "http://twitch.tv/ashuvidz/";
 
                         browser.browserAction.setIcon({path: "images/online.png"});
                         console.log("set icon online");
@@ -42,7 +45,7 @@
                 }
             );
         }
-
+        var linkUrl = "";
         twitchCheck();
 
         browser.alarms.create('twitchAlarm', {delayInMinutes: 1, periodInMinutes: 1});
@@ -51,10 +54,15 @@
             twitchCheck();
         });
 
-        function increment() {
+        browser.browserAction.onClicked.addListener(function()
+        {
+            browser.tabs.create({ url: linkUrl });
+        });
+
+        /*function increment() {
             browser.browserAction.setBadgeText({text: (13).toString()});
         }
 
-        increment()
+        increment()*/
     }
 )();
