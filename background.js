a6294d660ef4a2338fa19209801dfbcc10c2eee0
RONTEIX <vyrtualsynthese@gmail.com>
2018-04-13 11:43:51 +0200

    change the api call to use the new javascript fetch() function and drop jquery usage

--- background.js
+++ background.js
@@ -1,71 +1,98 @@
 (
     () => {
 
-        function onGot(item) {
+        // Bootstraps the plugin.
+        function bootstrap(item) {
+
+            // For the first time running after install. Set the status at 0 for non live.
             if (!item.isLive) {
                 browser.storage.local.set({
                     isLive:  "0"
                 });
-                buffer = browser.storage.local.get('isLive')
-                buffer.then(onGot, onError);
+
+                // To be sure that the variable is localy saved.
+                buffer = browser.storage.local.get('isLive');
+                buffer.then(bootstrap, onError);
             } else {
 
                 buffer.then(twitchCheck)
 
             }
-            let linkUrl = "";
         }
 
+        function linkManager(item) {
+            if (!item.linkUrl) {
+                browser.storage.local.set({
+                    linkUrl:  "https://youtube.com/ashuvidz/"
+                });
+
+                // To be sure that the variable is localy saved.
+                linkUrl = browser.storage.local.get('linkUrl');
+                linkUrl.then(linkManager, onError);
+            }
+        }
+
+        // Error manager for all localstorage try.
         function onError(error) {
             console.log(`Error: ${error}`);
         }
 
-        let buffer = browser.storage.local.get('isLive')
-        buffer.then(onGot, onError);
-
+        // This is the actual logic of the plugin.
         function twitchCheck(item){
-            $.ajax({
+
+
+            // Old methode to fetch twitch api using Jquery lib
+            /*$.ajax({
                 url: "https://api.twitch.tv/helix/streams?user_login=ashuvidz",
                 headers: { "Client-ID": "b90nfoacg9807542cq15o2qbv2g05q" },
-                type: "GET",
-                success: function() { console.log('Success!'); }
-            }).done(( channel ) => {
+                type: "GET"
+            }).done*/
 
+            // We first get data from Twitch Using Helix API using new nodejs fetch methode
+            // Setting the API Request to Twitch
+            const twitchUrl = 'https://api.twitch.tv/helix/streams?user_login=jvtv';
+            const twitchApiGetter = {
+                method: 'GET',
+                headers: { "Client-ID": "b90nfoacg9807542cq15o2qbv2g05q" }
+            };
+
+            // Fetching data from twitch API
+            fetch(twitchUrl, twitchApiGetter).then(( response ) => {
+                return response.json()}).then( (channel)=> {
                     //live is offline Youtube player appened
                     if (channel.data.length <= 0) {
-                        console.log(channel.data.length)
-                        browser.browserAction.setIcon({path: "images/offline.png"});
-                        console.log("set icon offline");
 
-                        linkUrl = "http://youtube.com/ashuvidz/";
-
+                        // If status didn't changed do Nothing
                         if (item.isLive === '0') return;
 
-                        browser.notifications.create('notifTwitchOnline', {
-                            'type': 'basic',
-                            'message': 'hi i am offline, bonne nuit ! ',
-                            'title': 'go to bed !!!'
-                        });
+                        // If status changed, change status back to not streaming and save it in local storage without notifications.
+                        browser.browserAction.setIcon({path: "images/offline.png"});
+
                         browser.storage.local.set({
-                            isLive:  "0"
+                            isLive:  "0",
+                            linkUrl: "http://youtube.com/ashuvidz/"
                         });
                         buffer = browser.storage.local.get('isLive')
 
-                        //live is online Twitch player
                     } else {
-                        linkUrl = "http://twitch.tv/ashuvidz/";
-
-                        browser.browserAction.setIcon({path: "images/online.png"});
-                        console.log("set icon online");
 
                         if (item.isLive === '1') return;
-                      browser.notifications.create('notifTwitchOffline', {
+
+                        // If live, append the link to Twitch and change images
+                        browser.browserAction.setIcon({path: "images/online.png"});
+
+                        // This sends a notification
+                        // TODO: Insert a link so when clicking on notification it open live stream channel in a new tab.
+                        browser.notifications.create('notifTwitchOffline', {
                             'type': 'basic',
                             'message': 'hi i am online, réveille toi ! ',
                             'title': 'here here here!!!'
                         });
+
+                        // Then update the local variable and get it again to ensure the promise is fired.
                         browser.storage.local.set({
-                            isLive:  "1"
+                            isLive:  "1",
+                            linkUrl: "http://twitch.tv/ashuvidz/"
                         });
                         buffer = browser.storage.local.get('isLive')
 
@@ -74,14 +101,25 @@
             );
         }
 
+        // This starts the plugin by geeting the saved previous status.
+        let linkUrl = browser.storage.local.get('linkUrl');
+        linkUrl.then(linkManager, onError);
+        let buffer = browser.storage.local.get('isLive');
+        buffer.then(bootstrap, onError);
+
+        // Borwser Event Listeners. This keep application running after bootstrap.
+        // This create the delay that trigger a new check every minutes
         browser.alarms.create('twitchAlarm', {delayInMinutes: 1, periodInMinutes: 1});
 
+        // This manage the behavior when an Alarm Event is Fired
         browser.alarms.onAlarm.addListener(() => {
             buffer.then(twitchCheck);
         });
 
+        // This manager the behavior when plugin icon is clicked
         browser.browserAction.onClicked.addListener(() =>
         {
+            console.log(linkUrl);
             browser.tabs.create({ url: linkUrl });
         });
 
